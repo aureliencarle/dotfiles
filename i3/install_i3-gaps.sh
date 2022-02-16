fd9a6929cd940f848d067ea9ae4d3c9d4456b8ce
aure <aurelien.carle.pro@gmail.com>
2022-02-16 19:33:27 +0100

    init need a lot of updates

---
+++ i3/install_i3-gaps.sh
@@ -0,0 +1,14 @@
+##
+# For Xubuntu
+##
+
+# shell and wallpapers
+sudo apt-get install rxvt-unicode feh
+
+# window_turn script
+sudo apt-get install libjson-perl
+
+# i3, gaps and awesome font for status
+sudo add-apt-repository ppa:regolith-linux/release
+sudo apt update
+sudo apt install i3 i3-gaps i3lock-fancy fonts-font-awesome
