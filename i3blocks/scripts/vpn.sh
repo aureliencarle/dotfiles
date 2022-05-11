df24682477bc2d331500c82dce5b7e9c8436bd14
Aurélien CARLE <aurelien.carle@cc.in2p3.fr>
2022-05-12 00:26:42 +0200

    update

--- i3blocks/scripts/vpn.sh
+++ i3blocks/scripts/vpn.sh
@@ -4,14 +4,14 @@
 bg="#7e7e7e" # grey darken-3
 icon=""
 if [ -d /proc/sys/net/ipv4/conf/tap0 ]; then
-    bg="#E53935" # rouge
+    bg="#FFFC00" # rouge
     icon=""
 fi
 separator $bg "#1976D2" # background left previous block
 ip=$(ifconfig tap0 | sed -n 's/.*inet \([0-9.]\+\).*/\1/p')
 
-echo " ${ip}"
-echo "${icon}"
+echo "🔑 ${ip}"
+echo "🔑 ${ip}"
 echo "$bg"
 
 
